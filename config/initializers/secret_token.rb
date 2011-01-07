15cb4ffb62fee182692f8d175a7ee6675560caa1
Mauricio Neira <mneira@siriusgenomics.com>
2011-01-07 11:20:33 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'a99cf590a9cbe3e0c7b6d8c075f641b98bdffba4a6f71852d39c20333593e27d586da291a7cdc6fccf664db2b305374c457dfcf657a45797d73ce5c9c5f3cb22'
